e316382c656ad33905ae1469027bdf7f68791a02
bpyj <57400978+bpyj@users.noreply.github.com>
2021-11-28 14:26:09 +0800

    script v112821-1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var allLi = document.getElementsByTagName("li");
-var allLiBtn = document.querySelectorAll(".remove");
+var allLi = document.getElementsByTagName("li"); //cache all li elements 
+var allLiBtn = document.querySelectorAll(".remove"); //cache all liBtn elements
 
 function inputLength() {
 	return input.value.length;
@@ -14,9 +14,10 @@ function createListElement() {
 	ul.appendChild(li);
 	input.value = "";
 	
+	//make a new delete btn 
 	makeNewDeleteBtn(li);
-	allLiBtn = document.querySelectorAll(".remove");
-
+	allLiBtn = document.querySelectorAll(".remove"); //update allLiBtn array again
+	//listen for li clicks, and deleteBtn clicks
 	clickLi();
 	clickDelete();
 }
@@ -38,11 +39,11 @@ button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
 
 //strikeout when item clicked
-
 function strikeOut(){
 	this.classList.toggle("done");
 }
 
+//listening to clicks on li
 function clickLi(){
 	for(var i=0; i<allLi.length; i++){
 		allLi[i].addEventListener("click", strikeOut)
@@ -55,7 +56,6 @@ function makeNewDeleteBtn(li){
 		deleteButton.appendChild(document.createTextNode("Delete"));		
 		deleteButton.classList.add("remove");
 		li.appendChild(deleteButton);
-	// return deleteButton;
 }
 
 //add delete button to existing li on start
@@ -67,14 +67,15 @@ function initAll(){
 		allLi[i].appendChild(deleteButton);		
 		deleteButton.classList.add("remove");
 	}
-	 allLiBtn = document.querySelectorAll(".remove");
+	 allLiBtn = document.querySelectorAll(".remove"); //update allLiBtn array
 }
 
-
+//remove li 
 function removeLi(){
 	this.parentNode.remove();
 }
 
+//listening to clicks on delete button
 function clickDelete(){
 	for(var i=0; i<allLiBtn.length; i++){
 		allLiBtn[i].addEventListener("click", removeLi);
